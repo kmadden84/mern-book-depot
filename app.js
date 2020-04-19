0566770e260e329155c834b147cde691aeb57a34
Kevin <kmadden84@gmail.com>
2020-04-19 11:47:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req,res) =>{
-    const index = path.join(__dirname, 'build', 'index.html');
+    const index = path.join(__dirname, 'client', 'build', 'index.html');
      res.sendFile(index);
   });
   
